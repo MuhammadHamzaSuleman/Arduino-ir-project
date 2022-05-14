2d8924e85ce83ce44e45e62ccd85922237d7f727
Muhammad hamza <102409025+muhammadhamza890@users.noreply.github.com>
2022-05-14 00:17:30 -0700

    IR project

---
+++ hamza_1_ir.ino
@@ -0,0 +1,20 @@
+const int ir = 2;
+const int led = 3;
+void setup() {
+  pinMode(ir, INPUT);
+  pinMode(led, OUTPUT); 
+  Serial.begin(9600);  
+}
+
+void loop() {
+  int sig = digitalRead(ir);
+  Serial.println(sig);
+  if (sig==LOW){
+    digitalWrite(led,HIGH);
+    Serial.println("LED IS ON");
+  }
+else{
+  digitalWrite(led,LOW);
+  Serial.println("LED IS OFF");
+}
+    }
